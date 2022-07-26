8b72cf588fcbf2187873951bf19f892b4045bfdc
Alexandr <sagder91@gmail.com>
2022-07-26 11:01:05 +0300

    fix Calculator

--- Calculator.java
+++ Calculator.java
@@ -2,29 +2,24 @@ public class Calculator {
     public static void main(String[] args) {
         int num1 = 5;
         int num2 = 2;
-        int result;
+        int result = 0;
         char sign = '^';
         if (sign == '+') {
             result = num1 + num2;
-            System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
         } else if (sign == '-') {
             result = num1 - num2;
-            System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
         } else if (sign == '*') {
             result = num1 * num2;
-            System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
         } else if (sign == '/') {
             result = num1 / num2;
-            System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
         } else if (sign == '%') {
             result = num1 % num2;
-            System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
         } else if (sign == '^') {
             result = 1;
             for (int i = 1; i <= num2; i++) {
                 result = result * num1;
             }
-            System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
         }
+        System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
     }
 }
